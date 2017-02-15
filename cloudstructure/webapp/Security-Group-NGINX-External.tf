174fa38190e94d0310431ae2f4afdb42bd132693
Mo Rawi <mo.rawi@capgemini.com>
2017-02-15 15:26:41 +0000

    this is the firewall

---
+++ cloudstructure/webapp/Security-Group-NGINX-External.tf
@@ -0,0 +1,12 @@
+resource "aws_security_group" "web_app" {
+  name = "Security-Group-NGINX-External"
+  description = "Firewall rules for the web server to App Servers."
+
+  ingress { 
+      from_port = 80
+      to_port = 5000
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
