875a73c87d2281480d44798351c3afe9825db864
Mo Rawi <mo.rawi@capgemini.com>
2017-02-17 10:30:33 +0000

    Have enhanced the web components with more specialization and added the EIP attachment to web server

--- cloudstructure/webapp/appserver02.tf
+++ cloudstructure/webapp/appserver02.tf
@@ -1,4 +1,10 @@
-resource "aws_instance" "appserver01" {
+resource "aws_instance" "web" {
   ami = "ami-c70525a1"
+  availability_zone= "eu-west-1"
   instance_type = "t2.micro"
+  tags {
+     Name = "appserver02"
+  }
 }
+
+
--- cloudstructure/webapp/nginx.tf
+++ cloudstructure/webapp/nginx.tf
@@ -1,4 +1,13 @@
-resource "aws_instance" "nginx" {
+resource "aws_instance" "web" {
   ami = "ami-ff022299"
+  availability_zone = "eu-west-1"
   instance_type = "t2.micro"
+  tags {
+     Name = "NGINX"
+  }
+}
+
+resource "aws_eip" "example" {
+   vpc = true
+
 }
