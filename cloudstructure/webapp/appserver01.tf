6ce3cb94b4146d0178b95ab4b8428dcd98ad9328
Mo Rawi <mo.rawi@capgemini.com>
2017-02-17 10:44:00 +0000

    Made changes to the webtag to stop repeat

--- cloudstructure/webapp/appserver01.tf
+++ cloudstructure/webapp/appserver01.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "app-server-01" {
   ami = "ami-c70525a1"
   availability_zone = "eu-west-1"
   instance_type = "t2.micro"
--- cloudstructure/webapp/nginx.tf
+++ cloudstructure/webapp/nginx.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "web-server" {
   ami = "ami-ff022299"
   availability_zone = "eu-west-1"
   instance_type = "t2.micro"
